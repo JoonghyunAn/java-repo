bd97f39fc9e505fd9f7b28439fd522787d620f63
JoonghyunAn <154599047+JoonghyunAn@users.noreply.github.com>
2023-12-26 21:30:55 +0900

    Add files via upload
    times table practice

---
+++ Fourth.java
@@ -0,0 +1,19 @@
+package introjava;
+
+public class Fourth {
+
+	public static void main(String[] args) 
+	{
+	
+		for(int first=1;first<=9;first++)
+		{
+			for(int second=1; second<=9; second++) 
+			{
+		System.out.println(first + " X " + second + " = " + first*second);
+
+			}
+
+		}
+
+	}
+}
